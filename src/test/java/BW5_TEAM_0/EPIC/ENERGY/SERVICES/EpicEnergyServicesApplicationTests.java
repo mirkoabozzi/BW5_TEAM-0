af3d1b948f02d925a021aaafb5564df9b6984be2
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-09-23 09:45:54 +0200

    Initial commit

---
+++ src/test/java/BW5_TEAM_0/EPIC/ENERGY/SERVICES/EpicEnergyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package BW5_TEAM_0.EPIC.ENERGY.SERVICES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicEnergyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
