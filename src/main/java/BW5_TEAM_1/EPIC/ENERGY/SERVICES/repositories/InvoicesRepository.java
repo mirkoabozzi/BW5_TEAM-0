1c291be9f87d3b6ce491a617cea30bc937447870
Andrea <ageria20@gmail.com>
2024-09-25 09:52:09 +0200

    filter By client method create - bug intellij

--- src/main/java/BW5_TEAM_1/EPIC/ENERGY/SERVICES/repositories/InvoicesRepository.java
+++ src/main/java/BW5_TEAM_1/EPIC/ENERGY/SERVICES/repositories/InvoicesRepository.java
@@ -1,6 +1,9 @@
 package BW5_TEAM_1.EPIC.ENERGY.SERVICES.repositories;
 
+import BW5_TEAM_1.EPIC.ENERGY.SERVICES.entities.Client;
 import BW5_TEAM_1.EPIC.ENERGY.SERVICES.entities.Invoice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,6 @@ import java.util.UUID;
 @Repository
 public interface InvoicesRepository extends JpaRepository<Invoice, UUID> {
     boolean existsByInvoiceNumber(int invoiceNumber);
+
+    Page<Invoice> findByClient(Pageable pageable, Client client);
 }
--- src/main/java/BW5_TEAM_1/EPIC/ENERGY/SERVICES/services/InvoicesService.java
+++ src/main/java/BW5_TEAM_1/EPIC/ENERGY/SERVICES/services/InvoicesService.java
@@ -44,6 +44,13 @@ public class InvoicesService {
         return this.invoicesRepository.findAll(pageable);
     }
 
+    // FILTER BY CLIENT
+    public Page<Invoice> filterByClients(int pages, int size, String sortBy) {
+        Client clientFormDb = this.clientsService.findByID();
+        Pageable pageable = PageRequest.of(pages, size, Sort.by(sortBy));
+        return this.invoicesRepository.findByClient(pageable);
+    }
+
     // PUT Update
     public Invoice updateInvoice(UUID id, InvoicesDTO payload) {
         Invoice invoiceFound = this.findByID(id);
