e50d919e475baa1d03dee07b50c976936eaa4537
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-09-25 10:53:49 +0200

    add csvReaderProvince

--- src/main/java/BW5_TEAM_1/EPIC/ENERGY/SERVICES/EpicEnergyServicesApplication.java
+++ src/main/java/BW5_TEAM_1/EPIC/ENERGY/SERVICES/EpicEnergyServicesApplication.java
@@ -1,13 +1,16 @@
 package BW5_TEAM_1.EPIC.ENERGY.SERVICES;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class EpicEnergyServicesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EpicEnergyServicesApplication.class, args);
     }
+
 
 }
