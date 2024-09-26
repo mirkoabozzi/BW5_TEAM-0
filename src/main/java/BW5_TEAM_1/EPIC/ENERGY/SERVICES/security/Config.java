bee4ba9de5c4a2f0a9a097a8d20927963c65416a
ageria20 <ageria20@gmail.com>
2024-09-26 10:16:42 +0200

    fe and be linked

--- src/main/java/BW5_TEAM_1/EPIC/ENERGY/SERVICES/security/Config.java
+++ src/main/java/BW5_TEAM_1/EPIC/ENERGY/SERVICES/security/Config.java
@@ -1,18 +1,28 @@
 package BW5_TEAM_1.EPIC.ENERGY.SERVICES.security;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 public class Config {
+
+    @Value("${cors.config.local.host}")
+    private String localHost;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -20,6 +30,21 @@ public class Config {
         httpSecurity.csrf(http -> http.disable());
         httpSecurity.sessionManagement(http -> http.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         httpSecurity.authorizeHttpRequests(http -> http.requestMatchers("/**").permitAll());
+        httpSecurity.cors(Customizer.withDefaults()); // OBBLIGATORIA QUESTA IMPOSTAZIONE SE VOGLIAMO CHE PER I CORS VENGA UTILIZZATO IL BEAN SOTTOSTANTE
         return httpSecurity.build();
     }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(localHost));
+        // whitelist con uno o più indirizzi dei frontend che voglio che accedano a questo backend. Se voglio permettere l'accesso a tutti
+        // (anche se un po' rischioso) basta mettere "*"
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    } // NON DIMENTICHIAMOCI CHE VA AGGIUNTA UN'IMPOSTAZIONE PER I CORS ANCHE NELLA FILTER CHAIN QUA SOPRA
 }
